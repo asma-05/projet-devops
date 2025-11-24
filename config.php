e44cbbbcb0d877bc2b60f2932f7031750966989b
ranimbs21 <ranimbenslimen06@gmail.com>
2025-11-24 09:21:22 +0100

    Remove comments from config.php

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
-// config.php
+
 $host = 'localhost';
 $dbname = 'mymelody';
 $username = 'root';
 $password = '';
 
-// Create connection
+
 $conn = mysqli_connect($host, $username, $password, $dbname);
 
-// Check connection
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
